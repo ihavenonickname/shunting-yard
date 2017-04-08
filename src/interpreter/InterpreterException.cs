7b579e8b1752e1da8fb5d8dad353f8eae4d2a05c
ihavenonickname <gbsm1993@gmail.com>
2017-04-08 17:01:24 -0300

    Adds error handling

---
+++ src/interpreter/InterpreterException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interpreter
+{
+    class InterpreterException : Exception
+    {
+        protected string message;
+
+        internal string FormattedMessage
+        {
+            get
+            {
+                return message;
+            }
+        }
+    }
+}
---
+++ src/interpreter/SyntaxException.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace interpreter
+{
+    internal class SyntaxException : InterpreterException
+    {
+        internal SyntaxException(string message)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine();
+            sb.AppendLine("Syntax error");
+            sb.AppendLine(message);
+
+            this.message = sb.ToString();
+        }
+    }
+}
